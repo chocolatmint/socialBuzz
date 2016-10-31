cfb028ce48bfcf9b235e8b4fd2e50aa7b91b3ff9
antoniusa15a@gmail.com <antoniusa15a@gmail.com>
2016-10-31 19:03:15 +0700

    twitter API buat library APInya, butuh ini

--- twit.php
+++ twit.php
@@ -1,22 +1,20 @@
 <?php
-session_start();
-//require_once("twitteroauth.php"); //Path to twitteroauth library
-require_once("twitteroauth/TwitterOAuth/TwitterOAuth.php");
-$twitteruser = "17919972";
-$notweets = 30;
-$consumerkey = "ViTIYnVPOPPUDFZtv1VtDyTBx";
-$consumersecret = "RJsnzqgGukzBK01JKHAaCa3iPeb2L4iYmyOi97sJZuraV14lUD";
-$accesstoken = "Y9suzaK9NNr0UnPtrfOoNH8wuCafNlv";
-$accesstokensecret = "C7hqRstTe3ZED6bCawhkXHXGDA75eUg9Kg7gTa5dnK6sK";
+require_once('TwitterAPIExchange.php');
+
+$settings = array(
+    'oauth_access_token' => "Y9suzaK9NNr0UnPtrfOoNH8wuCafNlv",
+    'oauth_access_token_secret' => "C7hqRstTe3ZED6bCawhkXHXGDA75eUg9Kg7gTa5dnK6sK",
+    'consumer_key' => "ECoBgGriuOKo3EzQi6Jt3HRS6",
+    'consumer_secret' => "to2p0M3RTZ4XMGiwnUmst1ZXXjOQXpVaQFoJDbBY70KvhJPU5M"
+);
  
-function getConnectionWithAccessToken($cons_key, $cons_secret, $oauth_token, $oauth_token_secret) {
-  $connection = new TwitterOAuth($cons_key, $cons_secret, $oauth_token, $oauth_token_secret);
-  return $connection;
-}
- 
-$connection = getConnectionWithAccessToken($consumerkey, $consumersecret, $accesstoken, $accesstokensecret);
- 
-$tweets = $connection->get("https://api.twitter.com/1.1/statuses/user_timeline.json?user_id=".$twitteruser."&count=".$notweets);
- 
-echo json_encode($tweets);
+$url = 'https://api.twitter.com/1.1/followers/ids.json';
+//url = 'https://api.twitter.com/1.1/users/show.json';
+$requestMethod = 'GET';
+//$getfield = '?screen_name=taylorswift13';
+$getfield = '?user_id=17919972';
+$twitter = new TwitterAPIExchange($settings);
+echo $twitter->setGetfield($getfield)
+    ->buildOauth($url, $requestMethod)
+    ->performRequest();
 ?>
